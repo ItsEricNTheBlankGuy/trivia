288ba9834df2113f281d9a0706ce49c915378c62
Eric <eric.nguyenblank@gmail.com>
2021-02-03 17:56:05 -0800

    stuff s

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,35 @@ import data from "../sample_data.json";
 
 function App() {
   var testQuestion = "test question";
+  var questionNum = 0;
   return (
     <div>
       <div className="app">Trivia!</div>
-      <Question text="Different Text" />
+      <Question text={data[0].question.text} />
+      <NextQuestion />
     </div>
   );
 }
 
 export default App;
 
-function Question({ text }) {
-  return <div>{text}</div>;
+function Question({ text, answerChoices} ) {
+  answerChoices.map();
+  return (
+    <div class="questionBox">
+      {text}
+      <Answer text={"Answer goes here"} {answerChoices} />
+      
+    </div>);
+}
+
+function NextQuestion( ) {
+  return (<button onclick="myFunction()">Click me</button>);
+}
+
+function Answer({ text }) {
+  return(
+  <div>
+    {text}
+  </div>);
 }
