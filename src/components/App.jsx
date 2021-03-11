beabeea68224eb6c73f9ab595febd07c53f0ffca
Debora Choi <54413436+deborachoi3@users.noreply.github.com>
2021-03-10 17:48:47 -0800

    trivia

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
+import Question from "./Question";
+import Answer from "./Answer";
+import NextQuestion from "./NextQuestion";
 
 function App() {
   let [questionNumber, changeQuestionNumber] = useState(0);
@@ -14,6 +17,7 @@ function App() {
   } else if (selectedAnswer !== "") {
     testQuestion = "Wrong";
   }
+console.log(questionNumber)
 
   return (
     <div>
@@ -22,10 +26,18 @@ function App() {
         text={data[questionNumber].question.text}
         answerChoices={data[questionNumber].question.choices}
         setChosenAnswer={changeSelectedAnswer}
-      />
+      /> 
 
       <NextQuestion
-        makeItNext={() => changeQuestionNumber(questionNumber + 1)}
+        makeItNext={() => {
+          if (questionNumber === 3) {
+            changeQuestionNumber(0)
+          }
+
+            else {
+            changeQuestionNumber(questionNumber + 1);
+          }
+        }}
       />
 
       <button
@@ -46,27 +58,31 @@ function App() {
 
 export default App;
 
-function Question({ text, answerChoices, setChosenAnswer }) {
+/* function Question({ text, answerChoices, setChosenAnswer }) {
   //answerChoices.map();
   return (
     <div class="questionBox">
       {text}
       <Answer text={"Answer goes here"} />
-      {answerChoices.map((answer) => (
+      {answerChoices.map((answer, i) => (
         <Answer
-          key={text}
+          key={i}
           text={answer}
           handleClick={() => setChosenAnswer(answer)}
         />
       ))}
     </div>
   );
-}
+} 
+*/
 
-function NextQuestion({ makeItNext }) {
+/* function NextQuestion({ makeItNext }) {
+/* function NextQuestion({ makeItNext }) {
   return <button onClick={makeItNext}>Next Question</button>;
 }
+*/
 
-function Answer({ text, handleClick }) {
+/* function Answer({ text, handleClick }) {
   return <div onClick={() => handleClick()}>{text}</div>;
 }
+*/
